d6435e75764065dbedf1afbec272fe21264d2481
Bill Shannon <bill@billshannon.com>
2015-11-13 11:57:05 -0700

    display all memories on a page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 var app = angular.module('memoriesApp', []);
-app.controller('MemoriesController', function ($scope) {
+app.controller('MemoriesController', function ($scope, $http) {
+
+    $http.get('http://memories-theory.cfapps.io/api/v1/memories').then(function (response) {
+      $scope.memories = response.data
+    })
 
 });
